4fb015267cad14e0029fc62c97337f519b1ea482
isalevine <isa.loves.coding@gmail.com>
2019-07-22 23:28:22 -0700

    refactoring with reusable object using let

--- testing.rb
+++ testing.rb
@@ -10,9 +10,10 @@ class Factorial
 end
 
 describe Factorial do
-    it "finds the factorial of 5" do
-        calculator = Factorial.new
+    let(:calculator) { Factorial.new }
 
+
+    it "finds the factorial of 5" do
         expect(calculator.factorial_of(5)).to eq(120)
     end
 end
