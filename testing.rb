600ed9b447b9b8e06dbefb15e8e2381cbbc9f15e
isalevine <isa.loves.coding@gmail.com>
2019-07-22 23:23:52 -0700

    first factorial test passing

--- testing.rb
+++ testing.rb
@@ -1,5 +1,18 @@
+# Tutorial from: https://www.rubyguides.com/2018/07/rspec-tutorial/
+
+
 require 'rspec/autorun'
 
+class Factorial
+    def factorial_of(n)
+        (1..n).inject(:*)
+    end
+end
+
 describe Factorial do
-    # ...
+    it "finds the factorial of 5" do
+        calculator = Factorial.new
+
+        expect(calculator.factorial_of(5)).to eq(120)
+    end
 end
